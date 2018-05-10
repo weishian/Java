754e00f59ced9e882ba86fd05a37e67c0184b8ea
DanielChang <daniel.chang@yxunistar.com>
2018-05-11 00:26:30 +0800

    ===Java How to Program=== add sample code

---
+++ src/ClassAverage2.java
@@ -0,0 +1,29 @@
+public class ClassAverage2
+{
+	public static void main(String[] args) {
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		
+		int total = 0;
+		int gradeCounter = 0;
+		
+		System.out.print("Enter grade or -1 to quit: ");
+		int grade = input.nextInt();
+		
+		while (grade != -1) {
+			total = total + grade;
+			gradeCounter = gradeCounter + 1;
+			
+			System.out.print("Enter grade or -1 to quit: ");
+			grade = input.nextInt();
+		}
+		
+		if (gradeCounter != 0) {
+			double average = (double) total / gradeCounter;
+			System.out.printf("%nTotal of the %d grades entered is %d%n", gradeCounter, total);
+			System.out.printf("Class average is %.2f%n", average);
+		}
+		else {
+			System.out.println("No grades were entered");
+		}
+	}
+}
